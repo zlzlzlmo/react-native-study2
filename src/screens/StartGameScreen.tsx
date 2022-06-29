b92290468bfe6137559f366202153401d5adb4e1
신승훈 <zlzlzlmo@daum.net>
2022-06-30 07:56:27 +0900

    text input 속성값 지정

--- src/screens/StartGameScreen.tsx
+++ src/screens/StartGameScreen.tsx
@@ -5,6 +5,7 @@ import {
   SafeAreaView,
   Platform,
   StatusBar,
+  TextInput,
 } from "react-native";
 import React from "react";
 import PrimaryButton from "../components/PrimaryButton";
@@ -12,10 +13,18 @@ import PrimaryButton from "../components/PrimaryButton";
 // * StyleSheet에 elevation 스타일 속성이 있는데 숫자가 높을 수록 box-shadow 효과를 보여준다  ( 안드로이드만 가능 ! )
 // ! IOS도 shadow효과를 주고 싶으면 shadowColor, shadowOffset, shadowOpacity, shadowRadius를 사용해야한다 ( 이것은 IOS만 가능 하기 때문에 두 플랫폼 모두 적용하려면 elevation과 이거 모두 적용 해야함 )
 
+// * TextInput에 있는 keyboardType 속성 값으로 인풋 입력시 사용할 키보드 타입을 지정할 수 있다.
+// * 또한 default 각 문장별로 첫번째 문자는 대문자로 쓰이는데 autoCapitalize 를 false를 주면 대문자로 변환하는걸 막을 수 있다.
 const StartGameScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.numberInput}
+          maxLength={2}
+          keyboardType="number-pad"
+          autoCapitalize="none"
+        />
         <PrimaryButton>버튼1</PrimaryButton>
         <PrimaryButton>버튼2</PrimaryButton>
       </View>
@@ -34,14 +43,26 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     borderRadius: 8,
-    backgroundColor: "blue",
+    padding: 5,
     elevation: 5,
-    shadowColor: "black",
+    shadowColor: "grey",
     shadowOffset: {
       width: 0,
       height: 2,
     },
-    shadowRadius: 6,
+    shadowRadius: 3.84,
     shadowOpacity: 0.25,
+    alignItems: "center",
+  },
+  numberInput: {
+    width: 50,
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
