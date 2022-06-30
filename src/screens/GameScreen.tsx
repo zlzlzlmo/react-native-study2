f58e8efb9dbf270c708dad6eba7d387e4224263d
신승훈 <zlzlzlmo@daum.net>
2022-06-30 12:50:38 +0900

    generateNextGuessNumber 기능 구현

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -3,10 +3,14 @@ import React, { useState } from "react";
 import { colors } from "../styles/variables";
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 interface GameScreenProps {
   userNumber: number;
 }
+
+let minBoundary = 1;
+let maxBoundary = 100;
 
 const GameScreen = ({ userNumber }: GameScreenProps) => {
   const generateRandomBetween = (
@@ -15,19 +19,46 @@ const GameScreen = ({ userNumber }: GameScreenProps) => {
     exclude: number
   ): number => {
     const randomNumber = Math.floor(Math.random() * (max - min)) + min;
-
     if (randomNumber === exclude) {
       return generateRandomBetween(min, max, exclude);
     }
     return randomNumber;
   };
-  const initialGuess = generateRandomBetween(1, 100, userNumber);
+
+  const initialGuess = generateRandomBetween(
+    minBoundary,
+    maxBoundary,
+    userNumber
+  );
   const [currentGuess, setCurrentGuess] = useState<number>(initialGuess);
+
+  const generateNextGuessNumber = (direction: "lower" | "greater") => {
+    if (direction === "lower") {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+
+    const newRandomNumber = generateRandomBetween(
+      minBoundary,
+      maxBoundary,
+      currentGuess
+    );
+
+    setCurrentGuess(newRandomNumber);
+  };
 
   return (
     <View style={styles.container}>
       <Title>Guess What?</Title>
       <NumberContainer>{currentGuess}</NumberContainer>
+      <View>
+        <Text>Higher or Lower?</Text>
+        <View>
+          <PrimaryButton onPress={() => {}}>+</PrimaryButton>
+          <PrimaryButton onPress={() => {}}>-</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 };
