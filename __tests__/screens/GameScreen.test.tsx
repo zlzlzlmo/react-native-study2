ef26af51da7c29f0ecca2598828e19d26c83e506
신승훈 <zlzlzlmo@daum.net>
2022-06-30 17:09:50 +0900

    game screen test code 케이스 추가

--- __tests__/screens/GameScreen.test.tsx
+++ __tests__/screens/GameScreen.test.tsx
@@ -3,21 +3,39 @@ import GameScreen from "../../src/screens/GameScreen";
 import { Alert } from "react-native";
 
 describe("<GameScreen/>", () => {
-  test("show alert when to click a wrong direction button", () => {
+  const gameOverMockFn = jest.fn();
+  let greaterBtn: any;
+  let lowerBtn: any;
+  beforeEach(() => {
     jest.spyOn(global.Math, "random").mockReturnValue(0.1);
     jest.spyOn(Alert, "alert");
-    const gameOverMockFn = jest.fn();
-    render(<GameScreen userNumber={25} onGameOver={gameOverMockFn} />);
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-    const greaterBtn = screen.getByText("+");
-    const lowerBtn = screen.getByText("-");
+  describe("userNumber is greater than currentGuess value", () => {
+    test("show alert when to click a wrong direction button", () => {
+      render(<GameScreen userNumber={25} onGameOver={gameOverMockFn} />);
 
-    expect(greaterBtn).toBeDefined();
-    expect(lowerBtn).toBeDefined();
+      greaterBtn = screen.getByText("+");
+      lowerBtn = screen.getByText("-");
 
-    fireEvent.press(lowerBtn);
-    expect(Alert.alert).toHaveBeenCalledWith("삐익!!", "솔직하게 답해주세요!", [
-      { style: "cancel", text: "다시 할게요!" },
-    ]);
+      fireEvent.press(lowerBtn);
+
+      expect(Alert.alert).toHaveBeenCalledWith(
+        "삐익!!",
+        "솔직하게 답해주세요!",
+        [{ style: "cancel", text: "다시 할게요!" }]
+      );
+    });
+
+    test("don't show alert when to click a right direction button", () => {
+      render(<GameScreen userNumber={25} onGameOver={gameOverMockFn} />);
+      greaterBtn = screen.getByText("+");
+      lowerBtn = screen.getByText("-");
+      fireEvent.press(greaterBtn);
+      expect(Alert.alert).not.toHaveBeenCalled();
+    });
   });
 });
